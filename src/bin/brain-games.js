7339eaa46b05ca2d73d58f328b003f69de59c1d2
german <88347@mail.ru>
2019-05-03 13:15:37 +0300

    18

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,7 +1,24 @@
 #!/usr/bin/node
 
-import * as askName from '..';
+import { request } from '..';
 
-console.log('Welcome to the Brain Games!\n');
-const name = askName('May I have your name? ');
-console.log(`Hello, ${name}!`);
+console.log('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".\n');
+const name = request('May I have your name? ');
+console.log(`Hello, ${name}!\n`);
+
+const f = (count) => {
+  if (count === 0) {
+    return `Congratulations, ${name}!`;
+  }
+  const randomInt = Math.floor(Math.random() * 100);
+  const correctAnswer = (randomInt % 2) ? 'no' : 'yes';
+  console.log(`Question: ${randomInt}`);
+  const answer = request('Your answer: ');
+  if (correctAnswer === answer) {
+    console.log('Correct!');
+    return f(count - 1);
+  }
+  return `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`;
+};
+
+console.log(f(3));
