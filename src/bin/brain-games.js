453fff8d3c78a48fc8b84082b6cfc4a0ef9c0fc9
german <88347@mail.ru>
2019-05-03 14:18:02 +0300

    err

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,24 +1,7 @@
 #!/usr/bin/node
 
-import { request } from '..';
+import * as askName from '..';
 
-console.log('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".\n');
-const name = request('May I have your name? ');
-console.log(`Hello, ${name}!\n`);
-
-const f = (count) => {
-  if (count === 0) {
-    return `Congratulations, ${name}!`;
-  }
-  const randomInt = Math.floor(Math.random() * 100);
-  const correctAnswer = (randomInt % 2) ? 'no' : 'yes';
-  console.log(`Question: ${randomInt}`);
-  const answer = request('Your answer: ');
-  if (correctAnswer === answer) {
-    console.log('Correct!');
-    return f(count - 1);
-  }
-  return `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`;
-};
-
-console.log(f(3));
+console.log('Welcome to the Brain Games!\n');
+const name = askName('May I have your name? ');
+console.log(`Hello, ${name}!`);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
 import readlineSync from 'readline-sync';
 
-const request = readlineSync.question;
-const pi = 3.14;
-
-export { request, pi }; /* export default неправильно проходит через babel.
-Также не проходит ф-я с 2-мя аргументами */
+export default readlineSync.question;
